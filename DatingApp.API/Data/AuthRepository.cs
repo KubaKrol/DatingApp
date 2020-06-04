63697cd625862a69235278f03dc8ddde8e0ffad6
Kubakrol <gosu.bis@gmail.com>
2020-06-04 20:06:41 +0200

    Introducing user registering

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public AuthRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public Task<User> Login(string username, string password)
+        {
+
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
