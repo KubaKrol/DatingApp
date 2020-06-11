d08ffa14ed32ae4a75fdad57cf82b5a7ce4254fc
Kubakrol <gosu.bis@gmail.com>
2020-06-11 18:40:18 +0200

    Introducing routing and routeGuards

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit
 {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit()
   {
@@ -22,10 +23,14 @@ export class NavComponent implements OnInit
     this.authService.login(this.model).subscribe(next =>
     {
       this.alertify.success('Logged in succesfully');
-    }, error =>
-    {
-      this.alertify.error('Failed to login');
-    });
+    },
+        error =>
+        {
+          this.alertify.error('Failed to login');
+        },
+        () => {
+          this.router.navigate(['/members']);
+        });
   }
 
   loggedIn()
@@ -37,6 +42,7 @@ export class NavComponent implements OnInit
   {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
 }
